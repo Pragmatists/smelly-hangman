a781f08f66dfd38eaa7fd053476c0f47bdbcd016
Pawel Barszcz <pawelbarszcz@gmail.com>
2016-02-17 14:02:08 +0100

    Tests: game reset

--- src/app/hangman.controller.spec.js
+++ src/app/hangman.controller.spec.js
@@ -1,10 +1,11 @@
 describe('HangmanController', function () {
 
   var initHangmanController
-    , scope;
+    , scope
+    , http;
 
   beforeEach(module('tdd'));
-  beforeEach(createScopeForController);
+  beforeEach(injectDependencies);
   beforeEach(createControllerInitializer);
 
   it('initializes', function () {
@@ -13,10 +14,63 @@ describe('HangmanController', function () {
     expect(hangman).toBeDefined();
   });
 
-  it('allows 6 misses', function () {
-    initHangmanController();
+  describe('on init', function () {
 
-    expect(scope.missesAllowed).toBe(6);
+    it('neither wins nor lost', function () {
+      initHangmanController();
+
+      expect(scope.win).toBeFalsy();
+      expect(scope.lost).toBeFalsy();
+    });
+
+    it('allows 6 misses', function () {
+      initHangmanController();
+
+      expect(scope.missesAllowed).toBe(6);
+    });
+
+    it('does not know about misses', function () {
+      initHangmanController();
+
+      expect(scope.numMisses).toBeUndefined();
+    });
+
+    it('does not know about secret word', function () {
+      initHangmanController();
+
+      expect(scope.secretWord).toBeUndefined();
+    });
+
+  });
+  describe('on reset', function () {
+
+    beforeEach(function () {
+      http.whenGET('/hangman/words').respond(200, {
+        words: ['apple']
+      });
+      initHangmanController();
+      http.flush();
+    });
+
+    it('neither wins nor lost', function () {
+      scope.reset();
+
+      expect(scope.win).toBeFalsy();
+      expect(scope.lost).toBeFalsy();
+    });
+
+    it('allows 6 misses', function () {
+      scope.reset();
+
+      expect(scope.missesAllowed).toBe(6);
+    });
+
+    it('set misses to 0', function () {
+      scope.reset();
+
+      expect(scope.numMisses).toBe(0);
+    });
+
   });
 
   it('knows available letters', inject(function (language) {
@@ -30,14 +84,16 @@ describe('HangmanController', function () {
     ]);
   }));
 
-  function createScopeForController() {
-    inject(function (_$rootScope_) {
-      scope = _$rootScope_.$new();
-    });
+
+  function injectDependencies() {
+    inject(function ($httpBackend) {
+      http = $httpBackend;
+    })
   }
 
   function createControllerInitializer() {
-    inject(function ($controller) {
+    inject(function ($controller, $rootScope) {
+      scope = $rootScope.$new();
       initHangmanController = function () {
         return $controller('HangmanController', {
           '$scope': scope
